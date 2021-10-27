f963182d583d0936f4b590a9e8fe7a1b4d0a216e
iq9891 <649076408@qq.com>
2021-10-27 11:36:59 +0800

    fix($icon): name error for endpoint square

---
+++ packages/img/src/icons/endpoint-square.ts
@@ -0,0 +1,24 @@
+/**
+ * @format
+ * @file EndpointSquare 方头节点
+ * @author 由 fe6 自动生成
+ */
+
+import { ISvgIconProps, IconWrapper } from '../runtime';
+
+// 获取 SVG 的 HTML 字符串
+export const getIconEndpointSquareSvgHtml = (props: ISvgIconProps) =>
+  `<?xml version="1.0" encoding="UTF-8"?>
+<svg width="${props.size}" height="${props.size}" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M43 24H10" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
+  <path d="M43 40H23.2857C20.0414 40 7 40 7 40L7 27" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
+  <path d="M43 8H23.2857C14.2914 8 7 8 7 8L7 21" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
+  <circle cx="7" cy="24" r="3" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
+</svg>`;
+
+// 默认导出组件
+export default IconWrapper(
+  'endpoint-square',
+  true,
+  getIconEndpointSquareSvgHtml,
+);
