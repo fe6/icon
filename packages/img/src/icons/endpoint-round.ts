241e109a0e8edd9d68ea7e7fa30da569a82bf63b
iq9891 <649076408@qq.com>
2021-10-27 11:46:04 +0800

    fix($icon): svg file error

--- packages/img/src/icons/endpoint-round.ts
+++ packages/img/src/icons/endpoint-round.ts
@@ -10,10 +10,10 @@ import { ISvgIconProps, IconWrapper } from '../runtime';
 export const getIconEndpointRoundSvgHtml = (props: ISvgIconProps) =>
   `<?xml version="1.0" encoding="UTF-8"?>
 <svg width="${props.size}" height="${props.size}" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M43 24H10" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
-  <path d="M43 40H23.2857C20.0414 40 7 40 7 40L7 27" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
-  <path d="M43 8H23.2857C14.2914 8 7 8 7 8L7 21" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
-  <circle cx="7" cy="24" r="3" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
+  <rect width="48" height="48" fill="white" fill-opacity="0.01"/>
+  <path d="M42 24L26 24" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
+  <circle cx="22" cy="24" r="3" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
+  <path d="M42 40H22C13.1634 40 6 32.8366 6 24C6 15.1634 13.1634 8 22 8H42" stroke="${props.colors[0]}" stroke-width="${props.strokeWidth}" stroke-linecap="${props.strokeLinecap}" stroke-linejoin="${props.strokeLinejoin}"/>
 </svg>`;
 
 // 默认导出组件
